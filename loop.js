0af450c9f041ed1479a81dbb46f55a33e7528676
Md. Moshiur Rahman <rahmanmoshiur253@gmail.com>
2021-08-22 11:54:43 +0600

    final commit

---
+++ loop.js
@@ -0,0 +1,152 @@
+var num = 10;
+
+// if (num % 2 == 0) {
+//     console.log("this number is even number");
+// } else {
+//     console.log("this number is odd number");
+// }
+
+// for (var index = 2; index < 20; index += 2) {
+//     console.log(index)
+// }
+//array
+// var StudentList = ['kamal', 'jamal', 'karim', 'rahim', 'jamil', 'manik', 'ratan', 'alal', 'dulal'];
+
+// var studentName = StudentList.indexOf('kamal');
+// var studentName = StudentList.indexOf('jamil');
+// var studentName = StudentList[3];
+// var studentName = StudentList[7];
+
+//condition\
+
+// var numbar = 12;
+
+// if (numbar % 2 == 0) {
+//     console.log(numbar, ' is even');
+// }
+// else {
+//     console.log(numbar, ' is odd');
+// }
+
+// leap year check 
+// var numbar = 1998;
+
+// if ((numbar % 4 == 0 && numbar % 100 !== 0) || numbar % 400 == 0) {
+//     console.log(numbar, ' is leap year');
+// }
+// else {
+//     console.log(numbar, ' is not leap year');
+// }
+
+// grade check 
+// var num = 101;
+
+// if (num <= -1 || num >= 101) {
+//     console.log("Your Score is out of range!!!!!");
+// }
+// else if (num >= 80 && num <= 100) {
+//     console.log("your grade is : A+");
+// }
+// else if (num >= 70 && num < 79) {
+//     console.log("your grade is : A");
+// }
+// else if (num >= 60 && num < 69) {
+//     console.log("your grade is : A-");
+// }
+// else if (num >= 50 && num < 59) {
+//     console.log("your grade is : B");
+// }
+// else if (num >= 40 && num < 49) {
+//     console.log("your grade is : C");
+// }
+// else if (num >= 33 && num < 39) {
+//     console.log("your grade is : D");
+// }
+// else {
+//     console.log("your grade is : F");
+// }
+
+// for loop
+// var StudentList = ['kamal', 'jamal', 'karim', 'rahim', 'jamil', 'manik', 'ratan', 'alal', 'dulal'];
+
+// for (let index = 0; index < StudentList.length; index++) {
+//     console.log(StudentList[index]);
+
+// }
+// var i = 0;
+//while loop
+// while (i <= StudentList.length) {
+//     console.log(StudentList[i]);
+//     i++;
+// }
+
+//do while loop
+// var i = 0;
+// var programminLanguageList = ['java', 'c', 'c++', 'python', 'html', 'c#', 'css', 'bootstap', 'database', 'react', 'php'];
+// do {
+//     console.log(programminLanguageList[i]);
+//     i++;
+// } while (i < programminLanguageList.length)
+
+
+//cgpa calculator using switch case
+
+// var gradNumbar = 89;
+
+// switch (gradNumbar) {
+//     case 'gradNumbar >= 80 && gradNumbar <= 100':
+//         console.log('A+');
+//         break;
+//     case 'gradNumbar >= 70 && gradNumbar <= 79':
+//         console.log('A');
+//         break;
+//     case 'gradNumbar >= 60 && gradNumbar <= 69':
+//         console.log('A-');
+//         break;
+//     case 'gradNumbar >= 50 && gradNumbar <= 59':
+//         console.log('B');
+//         break;
+
+//     default:
+//         console.log('F');
+//         break;
+// }
+
+// var color = 'green';
+
+// switch (color) {
+//     case 'blue':
+//         console.log('your color is blue');
+//         break;
+//     case 'red':
+//         console.log('your color is red');
+//         break;
+//     case 'yellow':
+//         console.log('your color is yellow');
+//         break;
+//     case 'green':
+//         console.log('your color is green');
+//         break;
+
+//     default:
+//         console.log('your color is black');
+//         break;
+// }
+
+
+//object 
+
+// var studentDetails = {
+//     id: 15303063,
+//     name: 'moshiur',
+//     email: 'rahmanmoshiur253@gmail.com',
+//     phone: 02903948,
+// };
+
+// // var phone = studentDetails.phone;
+// var phone = studentDetails['name'];
+// studentDetails.name = 'rahman';
+// studentDetails['phone'] = 01934622580;
+
+
+// console.log(studentDetails);
